386af0cd903197beae272fddb1c335fca0f1cd8b
aressss1 <codewithfaizi@gmail.com>
2023-09-16 19:53:07 +0500

    f- IP

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -50,7 +50,7 @@ const InviteCodePage  = async ({
     })
 
     if(server){
-        return redirect(`/server/${server.id}`)
+        return redirect(`/servers/${server.id}`)
     }
 
   return null
