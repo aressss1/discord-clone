42f8bd56f56246eb15b60712084b3af3eaa478bb
aressss1 <codewithfaizi@gmail.com>
2023-09-16 19:19:11 +0500

    -6

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,9 @@ import { CreateServerModal } from "../modals/create-server-modal"
 import { InviteModal } from "../modals/invite-modal"
 import { EditServerModal } from "../modals/edit-server-modal"
 import { MembersModal } from "../modals/members-modal"
+import { CreateChannelModal } from "../modals/create-channel-modal"
+import { LeaveServerModal } from "../modals/leave-server-modal"
+import { DeleteServerModal } from "../modals/delete-server-modal"
 
 export const ModalProvider = () => {
     const [ isMounted , setIsMounted ] = useState(false)
@@ -23,6 +26,9 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditServerModal />
             <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
         </>
     )
 }
--- components/servers/server-header.tsx
+++ components/servers/server-header.tsx
@@ -66,6 +66,7 @@ const ServerHeader = ({
             )}
             {isModerator && (
                 <DropdownMenuItem
+                    onClick={() => onOpen("createChannel")}
                     className="px-3 py-2 text-sm cursor-pointer"
                 >
                     Create Channel
@@ -77,6 +78,7 @@ const ServerHeader = ({
             )}
             {isAdmin && (
                 <DropdownMenuItem
+                    onClick={() => onOpen("deleteServer" , {server})}
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                 >
                     Delete Server
@@ -85,6 +87,7 @@ const ServerHeader = ({
             )}
             {!isAdmin && (
                 <DropdownMenuItem
+                    onClick={() => onOpen("leaveServer")}
                     className="px-3 py-2 text-sm cursor-pointer"
                 >
                     Leave Server     
