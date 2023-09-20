9b0b2c1032e7e69eb5a8860b8938bf9e5d272fde
aressss1 <codewithfaizi@gmail.com>
2023-09-20 20:21:02 +0500

    -9

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,49 @@
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
+interface ChannelPageProps {
+  params : {
+    serverId: string;
+    channelId: string
+  }
+}
+
+const ChannelIdPage = async ({
+  params
+}: ChannelPageProps) => {
+  const profile = await currentProfile()
+
+  if(!profile){
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId : params.serverId,
+      profileId: profile.id,
+    }
+  })
+
+  if(!channel || !member){
+    redirect("/")
+  }
+
   return (
-    <div>
-        Channel Id Page
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full" >
+        <ChatHeader 
+          name={channel.name}
+          serverId={channel.serverId}
+          type="channel"
+        />
     </div>
   )
 }
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import { Button } from "./ui/button"
+import NavigationSidebar from "./navigation/navigation-sidebar"
+import ServerSidebar from "./servers/server-sidebar"
+
+const MobileToggle = ({
+    serverId
+} : {
+    serverId : string
+}) => {
+  return (
+    <Sheet>
+        <SheetTrigger asChild >
+            <Button variant="ghost" size="icon" className="md:hidden" >
+                <Menu />
+            </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0 flex gap-0" >
+            <div className="w-[72px]" >
+                <NavigationSidebar />
+            </div>
+            <ServerSidebar serverId={serverId} />
+        </SheetContent>
+    </Sheet>
+    )
+}
+
+export default MobileToggle
